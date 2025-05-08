b2e37856acd6c1038b14711dfafeb8e873025029
Clown34435776 <kscubie@gmail.com>
2025-05-08 01:25:59 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+
 const app = express();
-app.get('/', (req, res) => res.send('Clowns-SMM Backend Running'));
-app.listen(3000, () => console.log('Backend running on port 3000'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'clowns-secret', resave: false, saveUninitialized: true }));
+app.use('/admin', express.static(path.join(__dirname, 'admin')));
+
+app.post('/admin/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'clownadmin' && password === 'Co11inQB!!!!!') {
+    req.session.loggedIn = true;
+    return res.redirect('/admin/dashboard');
+  }
+  res.send('Invalid credentials');
+});
+
+app.get('/admin/dashboard', (req, res) => {
+  if (req.session.loggedIn) {
+    return res.send('<h1>Welcome to the Admin Dashboard</h1><p>Coming soon...</p>');
+  }
+  res.redirect('/admin');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
